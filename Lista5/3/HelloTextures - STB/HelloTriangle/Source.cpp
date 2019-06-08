1269a7894599cec0ae7936333811fdac22ccb8e8
FabianeKuhn <FabianeKuhn@users.noreply.github.com>
2019-06-08 10:28:23 -0500

    Exercicio 3

---
+++ Lista5/3/HelloTextures - STB/HelloTriangle/Source.cpp
@@ -0,0 +1,29 @@
+/* Hello Transforms - código inicial fornecido em https://learnopengl.com/#!Getting-started/Hello-Triangle 
+ *
+ * Adaptado por Derick Martins Rentz
+ * Lista 05 - Exercício 03
+ * para a disciplina de Processamento Gráfico
+ * Versão inicial: 30/05/2019
+ * Última atualização em 06/06/2019
+ *
+ */
+
+using namespace std;
+
+#include "SceneManager.h"
+
+
+// The MAIN function, from here we start the application and run the game loop
+//int main()
+//{
+	//SceneManager *scene = new SceneManager;
+	//scene->initialize(800, 600);
+
+	//scene->run();
+
+	//scene->finish();
+	
+	//return 0;
+//}
+
+
